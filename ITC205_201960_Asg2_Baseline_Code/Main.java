3fc16b72d9817bc73ecc7d6ca6d18956f2d6b8b0
lahiru <lahiruru1@gmail.com>
2019-08-18 08:40:02 +0530

    used meaningful variable name for stringBuilder

--- ITC205_201960_Asg2_Baseline_Code/Main.java
+++ ITC205_201960_Asg2_Baseline_Code/Main.java
@@ -14,26 +14,26 @@ public class Main {
 	private static String Get_menu() {
 		StringBuilder stringBuilder = new StringBuilder(); // sb to stringBuilder. Meaningful variable name
 		
-		sb.append("\nLibrary Main Menu\n\n")
-		  .append("  M  : add member\n")
-		  .append("  LM : list members\n")
-		  .append("\n")
-		  .append("  B  : add book\n")
-		  .append("  LB : list books\n")
-		  .append("  FB : fix books\n")
-		  .append("\n")
-		  .append("  L  : take out a loan\n")
-		  .append("  R  : return a loan\n")
-		  .append("  LL : list loans\n")
-		  .append("\n")
-		  .append("  P  : pay fine\n")
-		  .append("\n")
-		  .append("  T  : increment date\n")
-		  .append("  Q  : quit\n")
-		  .append("\n")
-		  .append("Choice : ");
+		stringBuilder.append("\nLibrary Main Menu\n\n") // sb to stringBuilder. Meaningful variable name
+					 .append("  M  : add member\n")
+					 .append("  LM : list members\n")
+					 .append("\n")
+					 .append("  B  : add book\n")
+					 .append("  LB : list books\n")
+					 .append("  FB : fix books\n")
+					 .append("\n")
+					 .append("  L  : take out a loan\n")
+					 .append("  R  : return a loan\n")
+					 .append("  LL : list loans\n")
+					 .append("\n")
+					 .append("  P  : pay fine\n")
+					 .append("\n")
+					 .append("  T  : increment date\n")
+					 .append("  Q  : quit\n")
+					 .append("\n")
+					 .append("Choice : ");
 		  
-		return sb.toString();
+		return stringBuilder.toString(); // sb to stringBuilder. Meaningful variable name
 	}
 
 
