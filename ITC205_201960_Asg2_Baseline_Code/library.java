94fae00ba20f600284ef0f2948c2e8a9383789c1
yoshan369 <53467260+yoshan369@users.noreply.github.com>
2019-08-19 23:02:18 +1000

    Update library.java

--- ITC205_201960_Asg2_Baseline_Code/library.java
+++ ITC205_201960_Asg2_Baseline_Code/library.java
@@ -14,7 +14,7 @@ import java.util.List;
 import java.util.Map;
 
 @SuppressWarnings("serial")
-class library implements Serializable { // remove public syntx from infront of the class key word
+public class library implements Serializable { 
 	
 	private static final String libraryFile = "library.obj";
 	private static final int loanLimit = 2;
