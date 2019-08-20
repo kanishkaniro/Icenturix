58d757ef1cb177a54e51fd15c2fa77bafe17f7ca
yoshan369 <53467260+yoshan369@users.noreply.github.com>
2019-08-21 09:30:27 +1000

    AVAILABLE() change as available()
    AVAILABLE() change as available() according to standard

--- ITC205_201960_Asg2_Baseline_Code/book.java
+++ ITC205_201960_Asg2_Baseline_Code/book.java
@@ -42,7 +42,7 @@ public class book implements Serializable {
 
 
 	
-	public boolean AVAILABLE() { // AVAILABLE() change as available() according to standard
+	public boolean available() { // AVAILABLE() change as available() according to standard
 		return State == STATE.AVAILABLE;
 	}
 
