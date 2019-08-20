e38472a7a51419bc9ae575cc334f321c4940a1be
yoshan369 <53467260+yoshan369@users.noreply.github.com>
2019-08-20 17:59:58 +1000

    this.CALLNO change as this.callNo

--- ITC205_201960_Asg2_Baseline_Code/book.java
+++ ITC205_201960_Asg2_Baseline_Code/book.java
@@ -16,7 +16,7 @@ class book implements Serializable { // remove public syntx from infront of the 
 	public book(String author, String title, String callNo, int id) {
 		this.author = author;// AUTHOR change as author
 		this.title = title; // TITLE change as title 
-		this.CALLNO = callNo;
+		this.callNo = callNo;  // CALLNO change as callNo
 		this.ID = id;
 		this.State = STATE.AVAILABLE;
 	}
