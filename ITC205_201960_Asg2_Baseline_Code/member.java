cc6966649ede0367aa90bf5c917fef633f1a0793
yoshan369 <53467260+yoshan369@users.noreply.github.com>
2019-08-21 10:10:58 +1000

    EM change as email
    EM change as email according to standard

--- ITC205_201960_Asg2_Baseline_Code/member.java
+++ ITC205_201960_Asg2_Baseline_Code/member.java
@@ -9,7 +9,7 @@ public class member implements Serializable {
 
 	private String lastName; // LN change as lastName according to standard
 	private String firstName; // FN change as firstName according to standard
-	private String EM;
+	private String email; // EM change as email according to standard
 	private int PN;
 	private int ID;
 	private double FINES;
