9293e2e607c95b1984ca33a5fb60c7bf0c05730f
kanishkaniro <kanishkaniro@gmail.com>
2019-08-22 11:59:30 +1000

    Change enum names to Camel Case

--- ITC205_201960_Asg2_Baseline_Code/FixBookControl.java
+++ ITC205_201960_Asg2_Baseline_Code/FixBookControl.java
@@ -1,8 +1,8 @@
 public class FixBookControl {
 	
 	private FixBookUI UI;
-	private enum CONTROL_STATE { INITIALISED, READY, FIXING };
-	private CONTROL_STATE StAtE;
+	private enum ControlState { INITIALISED, READY, FIXING };// All enum names are to start with an upper case letter and to be in CamelBack. 
+	private ControlState StAtE;// All enum names are to start with an upper case letter and to be in CamelBack. 
 	
 	private library LIB;
 	private book Cur_Book;
@@ -10,22 +10,22 @@ public class FixBookControl {
 
 	public FixBookControl() {
 		this.LIB = LIB.INSTANCE();
-		StAtE = CONTROL_STATE.INITIALISED;
+		StAtE = ControlState.INITIALISED;// All enum names are to start with an upper case letter and to be in CamelBack. 
 	}
 	
 	
 	public void Set_Ui(FixBookUI ui) {
-		if (!StAtE.equals(CONTROL_STATE.INITIALISED)) {
+		if (!StAtE.equals(ControlState.INITIALISED)) {// All enum names are to start with an upper case letter and to be in CamelBack. 
 			throw new RuntimeException("FixBookControl: cannot call setUI except in INITIALISED state");
 		}	
 		this.UI = ui;
 		ui.Set_State(FixBookUI.UI_STATE.READY);
-		StAtE = CONTROL_STATE.READY;		
+		StAtE = ControlState.READY;// All enum names are to start with an upper case letter and to be in CamelBack. 		
 	}
 
 
 	public void Book_scanned(int bookId) {
-		if (!StAtE.equals(CONTROL_STATE.READY)) {
+		if (!StAtE.equals(ControlState.READY)) {// All enum names are to start with an upper case letter and to be in CamelBack. 
 			throw new RuntimeException("FixBookControl: cannot call bookScanned except in READY state");
 		}	
 		Cur_Book = LIB.Book(bookId);
@@ -40,12 +40,12 @@ public class FixBookControl {
 		}
 		UI.display(Cur_Book.toString());
 		UI.Set_State(FixBookUI.UI_STATE.FIXING);
-		StAtE = CONTROL_STATE.FIXING;		
+		StAtE = ControlState.FIXING;	// All enum names are to start with an upper case letter and to be in CamelBack. 	
 	}
 
 
 	public void FIX_Book(boolean MUST_fix) {
-		if (!StAtE.equals(CONTROL_STATE.FIXING)) {
+		if (!StAtE.equals(ControlState.FIXING)) {// All enum names are to start with an upper case letter and to be in CamelBack. 
 			throw new RuntimeException("FixBookControl: cannot call fixBook except in FIXING state");
 		}	
 		if (MUST_fix) {
@@ -53,12 +53,12 @@ public class FixBookControl {
 		}
 		Cur_Book = null;
 		UI.Set_State(FixBookUI.UI_STATE.READY);
-		StAtE = CONTROL_STATE.READY;		
+		StAtE = ControlState.READY;// All enum names are to start with an upper case letter and to be in CamelBack. 		
 	}
 
 	
 	public void SCannING_COMplete() {
-		if (!StAtE.equals(CONTROL_STATE.READY)) {
+		if (!StAtE.equals(ControlState.READY)) {// All enum names are to start with an upper case letter and to be in CamelBack. 
 			throw new RuntimeException("FixBookControl: cannot call scanningComplete except in READY state");
 		}	
 		UI.Set_State(FixBookUI.UI_STATE.COMPLETED);		
